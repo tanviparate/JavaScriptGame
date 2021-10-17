8aad7250e398fee3c1e38baad43953b94c1446a0
tanviparate <paratetanvi2@gmail.com>
2021-10-17 17:02:54 +0530

    strong attack added in branch node

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,25 @@
 
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE =17;
 const MONSTER_ATTACK_VALUE =14;
+
+
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler (){
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode){
+
+    let maxDamage;
+    if(mode == 'ATTACK'){
+        maxDamage =ATTACK_VALUE;
+    }else if (mode == 'STRONG_ATTACK'){
+        maxDamage =STRONG_ATTACK_VALUE;
+    }
+
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
@@ -20,8 +31,19 @@ function attackHandler (){
     }else if (currentMonsterHealth<=0 && currentPlayerHealth <=0){
         alert ("You have a draw !!");
     }
+
+}
+
+function attackHandler (){
+    attackMonster('ATTACK');
 }
 
 
+function strongAttackHandler(){
+    attackMonster('STRONG_ATTACK');
+
+}
+
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
